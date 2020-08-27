72cdc7362b8f99372eef23e7e57691a41549d419
Lucas Zampiva <32230014+lucas-zampiva@users.noreply.github.com>
2020-08-26 21:26:19 -0300

    Adiciona marcação do site dinâmico
    - Adicionado estrutura html para marcação do site.

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Projeto 01</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- para funcionar no tamanho de todos os dispositivos !-->
+
+		<!-- metatags para SEO !-->
+		<meta name="keywords" content="palavras-chave,do,meu,site"><!-- palavras chave !-->
+		<meta name="description" content="Descrição do meu website"><!-- para questões de SEO do site!-->
+		<!-- ------- !-->
+
+		<meta charset="utf-8"><!-- para questão de caracteres de acentuação !-->
+	</head>
+	<body>
+		<div class="center">
+			<header>
+				<div class="logo">Logomarca</div><!-- logo -->
+				<nav class="desktop">
+					<ul>
+						<li>
+							<a href="">Home</a>
+						</li>
+						<li>
+							<a href="">Sobre</a>
+						</li>
+						<li>
+							<a href="">Serviços</a>
+						</li>
+						<li>
+							<a href="">Contato</a>
+						</li>
+					</ul>
+				</nav>
+				<nav class="mobile">
+					<ul>
+						<li>
+							<a href="">Home</a>
+						</li>
+						<li>
+							<a href="">Sobre</a>
+						</li>
+						<li>
+							<a href="">Serviços</a>
+						</li>
+						<li>
+							<a href="">Contato</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+		</div>
+
+		<section class="banner-principal">
+			<div class="center">
+				<form>
+					<h2>Qual o seu melhor email?</h2>
+					<input type="email" name="email" required />
+					<input type="submit" name="acao" value="Cadastrar!">
+				</form>
+			</div>
+		</section><!-- banner-principal -->
+
+		<section class="descricao-autor">
+			<div class="center">
+				<div class="w50">
+					<h2>Lucas P. Zampiva</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+					</p>
+					<p>
+						Integer viverra finibus interdum. Aenean a suscipit dui. Praesent enim lectus, finibus ut rutrum et, imperdiet at leo. Nullam et nulla sem. Proin posuere elit ipsum, fermentum imperdiet mauris maximus id. Integer a tellus maximus nunc vestibulum placerat quis ultrices ex. Ut at urna neque. Etiam cursus ullamcorper luctus. Suspendisse interdum auctor elit non ultrices. Nulla porttitor, odio eu porta porta, metus orci mattis velit, et tincidunt orci mauris et neque. Integer at fringilla nisl. Suspendisse consectetur augue ac elit ornare, in luctus libero viverra. Curabitur maximus metus dictum tortor euismod porttitor. Sed porttitor lacinia cursus. Aliquam interdum lorem sed ligula accumsan consequat. Proin facilisis iaculis ligula, ac molestie est cursus vel.
+					</p>
+				</div>
+				<div class="w50">
+					<img src="images/foto.jpg" />
+				</div>
+			</div>
+		</section>
+
+		<section class="especialidades">
+			<h2 class="title">Especialidades</h2>
+			<div class="center">
+				<div class="box-especialidade">
+					<h3>Icone1</h3>
+					<h3>css3</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+					</p>
+				</div>
+				<div class="box-especialidade">
+					<h3>Icone2</h3>
+					<h3>javascript</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+					</p>
+				</div>
+				<div class="box-especialidade">
+					<h3>Icone3</h3>
+					<h3>html5</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+					</p>
+				</div>
+			</div>
+		</section><!--especialidades-->
+
+		<section class="extras">
+			<div class="center">
+				<div class="w50">
+					<h2 class="title">
+						Depoímentos
+					</h2>
+					<div class="depoimento-single">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+						</p>
+						<p class="nome-autor">Lorem ipsum</p>
+					</div>
+					<div class="depoimento-single">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+						</p>
+						<p class="nome-autor">Lorem ipsum</p>
+					</div>
+					<div class="depoimento-single">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+						</p>
+						<p class="nome-autor">Lorem ipsum</p>
+					</div>
+				</div>
+				<div class="w50">
+					<h2 class="title">
+						Serviços
+					</h2>
+					<ul>
+						<li>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+						</li>
+						<li>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+						</li>
+						<li>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec risus a ex mattis aliquam id at diam. Sed non scelerisque mauris. Mauris lacinia enim quis auctor hendrerit. Sed facilisis neque at feugiat consectetur. Sed tempor dui eu erat egestas accumsan. Vivamus suscipit ipsum eu sapien condimentum lacinia et in metus. Nam suscipit, quam ac cursus convallis, quam metus vehicula velit, vel efficitur purus libero non nibh. Proin non consequat diam. Fusce posuere mauris a cursus finibus. Maecenas vitae purus ac ex imperdiet cursus. Duis eu dictum diam, id laoreet lectus. Donec dapibus leo at magna pellentesque finibus. Pellentesque pretium, turpis ac fringilla malesuada, ante velit laoreet velit, sed bibendum est eros facilisis enim. Curabitur ac ipsum lectus. Vivamus in ornare turpis.
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+
+		<footer class="center">
+			<p>Todos os direitos reservados</p>
+		</footer>
+	</body>
+</html>
